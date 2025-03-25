3d52465c378733d948706869eb849e25d318c0f8
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-03-25 14:39:48 +0530

    Add files via upload

--- SecBAuth/index.js
+++ SecBAuth/index.js
@@ -9,29 +9,58 @@ server.use(e.urlencoded({ extended: true }));
 const path = require("path");
 
 mongoose
-  .connect("path to database")
+  .connect(
+    "mongodb+srv://piebytwo014:piebytwo014@cluster0.sy8lt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
-    console.log("connection success");
+    console.log("Database Connection Success");
   })
-  .catch(() => {
-    console.log("Some error occured");
+  .catch((err) => {
+    console.log(`Some Error in database ${err}`);
   });
 
+const userSchema = mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  isActive: Boolean,
+});
+
+const userModel = mongoose.model("user", userSchema);
+
 server.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "public", "index.html"));
-  //   C:\Users\ACER\Documents\SecBAuth\public\index.html
+  response.sendFile(path.join(__dirname, "public", "homepage.html"));
 });
 server.get("/login", (request, response) => {
   response.sendFile(path.join(__dirname, "public", "login.html"));
 });
-server.get("/logout", (request, response) => {});
 server.get("/register", (request, response) => {
   response.sendFile(path.join(__dirname, "public", "register.html"));
 });
-server.post("/create_new_user", (request, response) => {
-  console.log(request.body);
+server.post("/create-new-user", async (request, response) => {
+  try {
+    const { username, email, password } = request.body;
+
+    const exist = await userModel.findOne({ email: email });
+    if (exist) {
+      console.log("This email is associated with another account");
+    } else {
+      const newUser = await userModel({
+        username: username,
+        email: email,
+        password: password,
+        isActive: false,
+      });
+      await newUser.save();
+      console.log("Registration Success");
+    }
+  } catch (error) {
+    console.log("Registration Failed");
+  }
 });
-server.get("/reset_password", (request, response) => {});
+server.get("/*", (request, response) => {
+  response.sendFile(path.join(__dirname, "public", "fourOfour.html"));
+});
 
 server.listen(5252, () => {
   console.log("Server Started ");
